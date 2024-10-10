180b78307a2a59d4b940bfdf907a15ef59522ffa
aryu1217 <ryu200112@gmail.com>
2024-10-10 20:52:20 +0900

    🎉 random template 시작

--- src/main/kotlin/wp/practice/controller/SongController.kt
+++ src/main/kotlin/wp/practice/controller/SongController.kt
@@ -9,7 +9,7 @@ import wp.practice.service.SongService
 class SongController(private val service: SongService) {
     val title = listOf("노래1", "노래2", "노래3")
 
-    @GetMapping("/my_song/list")
+    @GetMapping("/song/list")
 //    fun getSongList(): List<String> {
 //        return title
 //    } -->를 간단화 하면 아래!
@@ -25,35 +25,6 @@ class SongController(private val service: SongService) {
 
     @GetMapping("/song/random")
     fun getRandomSong(): String {
-        val song = service.getRandomSong()
-
-        return """
-            <html>
-            <head>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-            </head>
-            <body>
-            <div class="card text-center">
-              <div class="card-header">
-                추천 노래
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">${song.title}</h5>
-                <p class="card-text">${song.singer}</p>
-                <a 
-                    href="https://www.youtube.com/results?search_query=노래방+${song.title}" 
-                    class="btn btn-primary">
-                    노래방
-                </a>
-              </div>
-              <a 
-                href="https://www.youtube.com/results?search_query=${song.title}" 
-                class="btn btn-secondary">
-                뮤직 비디오
-              </a>
-            </div>
-            </body>
-            </html>
-        """.trimIndent()
+        return "random"
     }
 }
